a26e0b2237dca36d2b3176985e6b9a227220d774
ETHAN KU <eyunku@gmail.com>
2024-01-31 19:16:10 -0600

    add Sym class

---
+++ Sym.java
@@ -0,0 +1,30 @@
+public class Sym {
+    private String type;
+
+    /**
+     * Constructor for the Sym (symbol) class. Initializes Sym to have
+     * the given type.
+     * 
+     * @param type  the type of the identifier, represented by a String
+     */
+    public Sym(String type) {
+        this.type = type;
+    }
+
+    /**
+     * Return this Sym's type.
+     * @return  the Sym's type.
+     */
+    public String getType() {
+        return type;
+    }
+
+    // TODO: proper toString method to be implemented when the stored type is more complex
+    /**
+     * Return this Sym's type.
+     * @return  the Sym's type.
+     */
+    public String toString() {
+        return type;
+    }
+}
